c3e54849eeda41e5726e329e3e44075ed9858e44
justin <justinaawd@gmail.com>
2024-11-14 05:54:44 -0800

    maybe finished debugging

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,19 +2,36 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 console.log("About outside");
-console.log("About.tsx outside window.JFCustomWidget", window.JFCustomWidget);
 console.log("About.tsx outside JFCustomWidget", JFCustomWidget);
 const About: React.FC = () => {
   console.log("About inside");
-  console.log("About.tsx inside window.JFCustomWidget", window.JFCustomWidget);
   console.log("About.tsx inside JFCustomWidget", JFCustomWidget);
 
-  const wd = window.JFCustomWidget.getWidgetData();
-  console.log("widgedata", wd);
-  window.JFid = wd.formID;
-  console.log("JFid", window.JFid);
-  const d = window.JFCustomWidget.getWidgetSetting("date");
-  console.log("date", d);
+  JFCustomWidget.subscribe("ready", function () {
+    console.log("/about READY");
+    const d = JFCustomWidget.getWidgetSetting("index.html date");
+    console.log("/about date", d);
+    const settings = JFCustomWidget.getWidgetSettings();
+    console.log("/about settings", settings);
+    const wd = JFCustomWidget.getWidgetData();
+    console.log("/about widgedata", wd);
+
+    // const lt = document.getElementById("labelText") as HTMLElement;
+    // lt.innerHTML = label;
+    //subscribe to form submit event
+    JFCustomWidget.subscribe("submit", function () {
+      console.log("SUBMITTING");
+      // const ui = document.getElementById("userInput") as HTMLInputElement;
+      const msg = {
+        //you should valid attribute to data for JotForm
+        //to be able to use youw widget as required
+        valid: true,
+        value: "VALUE",
+      };
+      // send value to JotForm
+      JFCustomWidget.sendSubmit(msg);
+    });
+  });
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
